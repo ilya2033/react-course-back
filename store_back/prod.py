5e9498ac4d4034b6d99485f3d34507bbfea8726c
ilya_shyian <ilya.shyian@gmail.com>
2022-07-07 12:36:27 +0300

    try to fix cors

--- store_back/prod.py
+++ store_back/prod.py
@@ -4,6 +4,6 @@ from datetime import timedelta
 
 
 CSRF_TRUSTED_ORIGINS = ["http://188.72.209.29"]
-ALLOWED_HOSTS = ["http://188.72.209.29"]
+ALLOWED_HOSTS = ["http://188.72.209.29","127.0.0.1","http://127.0.0.1:8000"]
 SECURE_HSTS_SECONDS = 2_592_000
 
