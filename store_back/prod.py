87a76cec52c91e82142a26a84dc108c283dfcc39
ilya_shyian <ilya.shyian@gmail.com>
2022-07-07 12:02:13 +0300

    change allowed_hosts

--- store_back/prod.py
+++ store_back/prod.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from .settings import SECRET_KEY,BASE_DIR,PROJECT_DIR,DEBUG
 
 
-CSRF_TRUSTED_ORIGINS = []
-ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = ["http://188.72.209.29"]
+ALLOWED_HOSTS = ["http://188.72.209.29"]
 SECURE_HSTS_SECONDS = 2_592_000
 
