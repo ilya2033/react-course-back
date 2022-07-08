7200c0054b0ed1cc08855f14f6cb7c9697390f92
ilya_shyian <ilya.shyian@gmail.com>
2022-07-08 15:48:14 +0300

    remove print

--- store_back/apps/goods/schema.py
+++ store_back/apps/goods/schema.py
@@ -22,7 +22,6 @@ class ImageType(graphene.ObjectType):
         return self._id
 
     def resolve_url(self,info):
-        print(settings.MEDIA_URL + str(self.url))
         return settings.MEDIA_URL + str(self.url)
 
 
